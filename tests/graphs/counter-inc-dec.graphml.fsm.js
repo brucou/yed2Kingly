96674109c89b8eb300ed4a737747ae420914be36
b <bruno.couriol@gmail.com>
2020-12-29 14:42:46 +0100

    chore: added scripts to package.json to run tests, and committed missing test files

---
+++ tests/graphs/counter-inc-dec.graphml.fsm.js
@@ -0,0 +1,150 @@
+import { createStateMachine } from "kingly";
+
+// Copy-paste help
+// For debugging purposes, guards and actions functions should all have a name
+// Using natural language sentences for labels in the graph is valid
+// guard and action functions name still follow JavaScript rules though
+// -----Guards------
+/**
+ * @param {E} extendedState
+ * @param {D} eventData
+ * @param {X} settings
+ * @returns Boolean
+ */
+// const guards = {
+//   "is it": function (){},
+//   "is it not": function (){},
+// };
+// -----Actions------
+/**
+ * @param {E} extendedState
+ * @param {D} eventData
+ * @param {X} settings
+ * @returns {{updates: U[], outputs: O[]}}
+ * (such that updateState:: E -> U[] -> E)
+ */
+// const actions = {
+//   "increment counter": function (){},
+//   "render": function (){},
+//   "decrement counter": function (){},
+//   "render some more": function (){},
+// };
+// ----------------
+function contains(as, bs) {
+  return as.every(function (a) {
+    return bs.indexOf(a) > -1;
+  });
+}
+
+function chain(arrFns, actions) {
+  return function chain_(s, ed, stg) {
+    return arrFns.reduce(
+      function (acc, fn) {
+        var r = actions[fn](s, ed, stg);
+
+        return {
+          updates: acc.updates.concat(r.updates),
+          outputs: acc.outputs.concat(r.outputs),
+        };
+      },
+      { updates: [], outputs: [] }
+    );
+  };
+}
+
+function every(arrFns, guards) {
+  return function every_(s, ed, stg) {
+    return arrFns.reduce(function (acc, fn) {
+      var r = guards[fn](s, ed, stg);
+
+      return r && acc;
+    }, true);
+  };
+}
+
+var NO_OUTPUT = [];
+var NO_STATE_UPDATE = [];
+var events = ["click inc", "click dec"];
+var states = { n1ღidle: "" };
+function getKinglyTransitions(record) {
+  var aF = record.actionFactories;
+  var guards = record.guards;
+  var actionList = ["increment counter", "render", "decrement counter", "render some more"];
+  var predicateList = ["is it", "is it not"];
+  if (!contains(actionList, Object.keys(aF))) {
+    console.error(
+      "Some actions are missing either in the graph, or in the action implementation object! Cf actionFactories (you passed that) vs. actionList (from the graph) below. They must have the same items!"
+    );
+    console.error({ actionFactories: Object.keys(aF), actionList });
+    var passedAndMissingInGraph = Object.keys(aF).filter(function (k) {
+      return actionList.indexOf(k) === -1;
+    });
+    passedAndMissingInGraph.length > 0 &&
+      console.error(
+        "So the following actions were passed in parameters but do not match any action in the graph! This may happen if you modified the name of an action in the graph, but kept using the older name in the implementation! Please check.",
+        passedAndMissingInGraph
+      );
+    var inGraphButNotImplemented = actionList.filter(function (k) {
+      return Object.keys(aF).indexOf(k) === -1;
+    });
+    inGraphButNotImplemented.length > 0 &&
+      console.error(
+        "So the following actions declared in the graph are not implemented! Please add the implementation. You can have a look at the comments of the generated fsm file for typing information.",
+        inGraphButNotImplemented
+      );
+    throw new Error(
+      "Some actions implementations are missing either in the graph, or in the action implementation object!"
+    );
+  }
+  if (!contains(predicateList, Object.keys(guards))) {
+    console.error({ guards: Object.keys(guards), predicateList });
+    throw new Error("Some guards are missing either in the graph, or in the guard implementation object!");
+  }
+  const transitions = [
+    { from: "nok", event: "init", to: "n1ღidle", action: chain([], aF) },
+    {
+      from: "n1ღidle",
+      event: "click inc",
+      guards: [
+        {
+          predicate: every(["is it", "is it not"], guards),
+          to: "n1ღidle",
+          action: chain(["increment counter", "render"], aF),
+        },
+      ],
+    },
+    {
+      from: "n1ღidle",
+      event: "click dec",
+      to: "n1ღidle",
+      action: chain(["decrement counter", "render", "render some more"], aF),
+    },
+  ];
+
+  return transitions;
+}
+
+function createStateMachineFromGraph(fsmDefForCompile, settings) {
+  var updateState = fsmDefForCompile.updateState;
+  var initialExtendedState = fsmDefForCompile.initialExtendedState;
+
+  var transitions = getKinglyTransitions({
+    actionFactories: fsmDefForCompile.actionFactories,
+    guards: fsmDefForCompile.guards,
+  });
+
+  var fsm = createStateMachine(
+    {
+      updateState,
+      initialExtendedState,
+      states,
+      events,
+      transitions,
+    },
+    settings
+  );
+
+  return fsm;
+}
+
+export { events, states, getKinglyTransitions, createStateMachineFromGraph };
